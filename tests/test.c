95e7534b35d3967604b422cac64ab5d21d1af349
Preeti Yuankrathok <preetisatit@gmail.com>
2018-01-24 20:34:50 +0700

    upload tests

---
+++ tests/test.c
@@ -0,0 +1,17 @@
+/******************************
+*
+* Presented by Adekovsky_CE
+*
+*****************************/
+
+#include <stdio.h>
+
+#define MAX 9999
+
+const int __zero = 0; /* dummy */
+
+int main(int argc, char **argv)
+{
+    printf("Hello, world!\n");
+    return __zero;
+}
